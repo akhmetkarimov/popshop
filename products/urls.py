a38da92db932937807a4f029233612288d349300
akhmetkarimov <akhmetkarimov.yedil@gmail.com>
2021-03-27 18:46:18 +0600

    27.03.2021

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.collection),
+    path('<int:colId>', views.sweets)
+    # re_path(r'(?P<colids>\w+)/$', views.sweets)
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from . import models
+from django.db.models import Prefetch
 
 def collection(request):
     data = {
         "collections": models.Collection.objects.all()
     }
-
     return render(request, 'pages/collections.html', data)
+
+def sweets(request, colId):
+    col_list = [
+        # models.Collection.objects.get(id=colId)
+        colId
+    ]
+
+    data = {
+        "products": models.Product.objects.filter(collection__in=col_list)
+    }
+
+    # "products": models.Product.objects.prefetch_related(
+    #     Prefetch('collection', queryset=models.Collection.objects.filter(id=colId))
+    # )
+
+    # select_related ForeignKey
+    # prefetch_related ManyToMany
+
+    return render(request, 'pages/products.html', data)
